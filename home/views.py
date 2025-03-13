a0683e1aa2b0f2f4e42153c9dc48750928a0f82b
fenny7646 <fennyvarghese1@gmail.com>
2025-03-14 02:57:56 +0530

    Added itemdesc.html

--- home/views.py
+++ home/views.py
@@ -3,7 +3,4 @@ from django.shortcuts import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'Base/base.html')
-
-def index(request):
-    return HttpResponse("hey")
+    return render(request, 'Base/itemdesc.html')
