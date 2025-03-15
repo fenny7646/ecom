a054c43e84ea0625c4c9d2a55ef11dd76a4c3b45
fenny7646 <fennyvarghese1@gmail.com>
2025-03-15 19:34:07 +0530

    Adder.html added

--- home/views.py
+++ home/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'Base/wishlist.html')
+    return render(request, 'Base/adder.html')
